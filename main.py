7331f8badae37ead5fbbd03aadc55ff2be4813ba
GonzalezLE <gonzalez-3216@hotmail.com>
2022-09-16 23:33:52 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from fastapi import FastAPI
+#pyhon
+from typing import Optional,Dict
 
+#PYdantic
+from pydantic import BaseModel
+
+#FastAPI
+from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#model
+
+class Person(BaseModel):
+    first_name : str
+    last_name :str
+    age :int
+    hair_color :Optional[str] = None
+    is_married :Optional[bool] = None
 
 # path operation decoretor
 @app.get('/')
 def home():
     return {"Hello":"world"}
 
+# Request and Response Body
+@app.post('person/new')
+def create_person(person: Person = Body(...)):
+    return person
