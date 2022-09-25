b11520614be24ead9ff096209bd2208095a41daf
GonzalezLE <gonzalez-3216@hotmail.com>
2022-09-24 22:17:57 -0500

    validations_models

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #pyhon
+
 from typing import Optional,Dict
-from unittest import result
+from enum import Enum
 
 #PYdantic
 from pydantic import BaseModel
+from pydantic import Field
 
 #FastAPI
 from fastapi import FastAPI
@@ -12,6 +14,13 @@ from fastapi import Body,Query,Path
 app = FastAPI()
 
 #model
+class HairColor(Enum):
+    white = 'white'
+    brown = 'brown'
+    black = 'black'
+    blonde = 'blonde'
+    red = 'red'
+    
 
 class Location(BaseModel):
     city : str
@@ -19,11 +28,23 @@ class Location(BaseModel):
     country : str
 
 class Person(BaseModel):
-    first_name : str
-    last_name :str
-    age :int
-    hair_color :Optional[str] = None
-    is_married :Optional[bool] = None
+    first_name : str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+        )
+    last_name :str= Field(
+        ...,
+        min_length=1,
+        max_length=50
+        )
+    age :int = Field(
+        ...,
+        gt=0,
+        le=115
+        )
+    hair_color :Optional[HairColor] = Field(default=None)
+    is_married :Optional[bool] = Field(default=None)
 
 # path operation decoretor
 @app.get('/')
